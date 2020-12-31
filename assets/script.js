e60d2c8b47a45fd8aff47c6481493f790ada1701
Jarrett Dougherty <jarrettdougherty@gmail.com>
2020-12-30 20:05:35 -0500

    add contact page. add navbar dropdown

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+document.getElementById("contact-send-button").addEventListener("click", ()=>{
+    console.log('submit');
+    sendMail();
+});
+
+const sendMail = () => {
+    const email = 'jarrettdougherty@gmail.com';
+    const userName = document.getElementById('contactName').value;
+    const subject = `Contact MoodConnect. Message from ${userName}`;
+    const emailBody = document.getElementById('contactBody').value;
+
+    const link = 'mailto:' + email + '?subject=' + subject + "&body=" + emailBody;
+
+    window.location.href = link;
+};
